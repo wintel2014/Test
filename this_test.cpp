95e937555f515314ce3e5e9a599b76b68c7a2fdb
wintel@2014 <jiazi269@126.com>
2014-03-12 15:06:00 +0800

    Test for merge 2

---
+++ this_test.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class Base {
+    public:
+       // Base* printThisPointer(){cout<<"*******"<<this<<"*********"<<endl;}  //sizeof=1
+       virtual Base* printThisPointer(){cout<<"*******"<<this<<"*********"<<endl;} //sizeof=8
+};
+class A{
+    public:
+        A(Base* );
+    private:
+        Base* pB;
+};
+
+A::A(Base* p){
+    pB=p;pB->printThisPointer();
+    cout<<"*******"<<this<<"*********in Class A"<<endl;
+}
+int main()
+{
+    Base b;
+    Base* pB=&b;
+    cout<<"*******"<<pB<<"*********"<<sizeof(Base)<<endl; 
+    Base* pB2=b.printThisPointer();
+
+    A a(pB);
+    cout<<"Class A:*******"<<&a<<"*********"<<endl;
+    long long* longP=(long long*)&a;
+    cout<<"********0x"<<hex<<*longP<<"********"<<endl;
+    cout<<"********0x"<<hex<<*(longP+1)<<"********"<<endl;
+    
+}
